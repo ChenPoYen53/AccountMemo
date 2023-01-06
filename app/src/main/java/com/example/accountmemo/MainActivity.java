d6105e21567f7948fdc5056d36defa375d6c6424
ChenPoYen53 <120180563+ChenPoYen53@users.noreply.github.com>
2023-01-06 18:19:31 +0800

    android:screenOrientation="portrait" Change Icon

--- app/src/main/java/com/example/accountmemo/MainActivity.java
+++ app/src/main/java/com/example/accountmemo/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
+        setTitle("");
+
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
